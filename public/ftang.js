b4e4d20cb758baf965f62a0d236b60fc892d5441
Max Ogden <mogden@gmail.com>
2009-12-31 17:22:29 -0800

    refactoring and namespacing javascript

--- public/ftang.js
+++ public/ftang.js
@@ -1,9 +1,39 @@
 // documentation for this version of jplayer: http://www.happyworm.com/jquery/jplayer/0.2.5/developer-guide.htm
 var myPlayList = "";
 var playItem = 0;
-var Playlist = {
+var FTANGPlayer = {
+	initJPlayer: function() {
+    $("#jquery_jplayer").jPlayer({ 
+			ready: function() { FTANGPlayer.loadPlaylist(); }, 
+			oggSupport: false
+		})
+    	.jPlayerId("play", "player_play")
+	    .jPlayerId("pause", "player_pause")
+	    .jPlayerId("stop", "player_stop")
+	    .jPlayerId("loadBar", "player_progress_load_bar")
+	    .jPlayerId("playBar", "player_progress_play_bar")
+	    .jPlayerId("volumeMin", "player_volume_min")
+	    .jPlayerId("volumeMax", "player_volume_max")
+	    .jPlayerId("volumeBar", "player_volume_bar")
+	    .jPlayerId("volumeBarValue", "player_volume_bar_value")
+	    .onProgressChange( function(loadPercent, playedPercentRelative, playedPercentAbsolute, playedTime, totalTime) {
+	      var myPlayedTime = new Date(playedTime);
+	      var ptMin = (myPlayedTime.getUTCMinutes() < 10) ? "0" + myPlayedTime.getUTCMinutes() : myPlayedTime.getUTCMinutes();
+	      var ptSec = (myPlayedTime.getUTCSeconds() < 10) ? "0" + myPlayedTime.getUTCSeconds() : myPlayedTime.getUTCSeconds();
+	      $("#play_time").text(ptMin+":"+ptSec);
+
+	      var myTotalTime = new Date(totalTime);
+	      var ttMin = (myTotalTime.getUTCMinutes() < 10) ? "0" + myTotalTime.getUTCMinutes() : myTotalTime.getUTCMinutes();
+	      var ttSec = (myTotalTime.getUTCSeconds() < 10) ? "0" + myTotalTime.getUTCSeconds() : myTotalTime.getUTCSeconds();
+	      $("#total_time").text(ttMin+":"+ttSec);
+	    })
+	    .onSoundComplete( function() {
+	     FTANGPlayer.playListNext();
+	    })
+	},
+
 	displayPlayList: function() {
-		Playlist.playListInit(false);
+		FTANGPlayer.playListInit(false);
 		$('#playlist_list ul').empty();
 	  for (i=0; i < myPlayList.length; i++) {
 	    $("#playlist_list ul").append("<li id='playlist_item_"+i+"'>"+ myPlayList[i].name +"</li>");
@@ -19,7 +49,7 @@ var Playlist = {
 	    ).click( function() {
 	      var index = $(this).data("index");
 	      if (playItem != index) {
-	        Playlist.playListChange( index );
+	       FTANGPlayer.playListChange( index );
 	      } else {
 	        $("#jquery_jplayer").play();
 	      }
@@ -28,7 +58,7 @@ var Playlist = {
 	    $("#playlist_item_" + i).append("<span id='playlist_remove_item_"+i+"' class='playlist_remove'>"+ 'rm' +"</span>");
 	    $("#playlist_remove_item_"+i).click( function() {
 	      var index = $(this).attr('id').split('_').pop();
-	      Playlist.playListRemove(index);
+	     FTANGPlayer.playListRemove(index);
 	      return false;
 	    });
 	  }
@@ -36,9 +66,9 @@ var Playlist = {
 
 	playListInit: function(autoplay) {
 	  if(autoplay) {
-	    Playlist.playListChange( playItem );
+	   FTANGPlayer.playListChange( playItem );
 	  } else {
-	    Playlist.playListConfig( playItem );
+	   FTANGPlayer.playListConfig( playItem );
 	  }
 	},
 
@@ -52,18 +82,18 @@ var Playlist = {
 	},
 
 	playListChange: function( index ) {
-	  Playlist.playListConfig( index );
+	 FTANGPlayer.playListConfig( index );
 	  $("#jquery_jplayer").play();
 	},
 
 	playListNext: function() {
 	  var index = (playItem+1 < myPlayList.length) ? playItem+1 : 0;
-	  Playlist.playListChange( index );
+	 FTANGPlayer.playListChange( index );
 	},
 
 	playListPrev: function() {
 	  var index = (playItem-1 >= 0) ? playItem-1 : myPlayList.length-1;
-	  Playlist.playListChange( index );
+	 FTANGPlayer.playListChange( index );
 	},
 
 	playListRemove: function(song) {
@@ -82,85 +112,63 @@ var Playlist = {
 	  if(playItem > song) {
 	    playItem--;
 	  }
-	}
-}
-
-$( function() {
-  function init_jplayer (playlist) {
-    myPlayList = playlist;
-    $("#jquery_jplayer").jPlayer({
-      ready: function() {
-        $('#playlist_list ul').empty();
-        Playlist.displayPlayList();
-      },
-      oggSupport: false
-    })
-    .jPlayerId("play", "player_play")
-    .jPlayerId("pause", "player_pause")
-    .jPlayerId("stop", "player_stop")
-    .jPlayerId("loadBar", "player_progress_load_bar")
-    .jPlayerId("playBar", "player_progress_play_bar")
-    .jPlayerId("volumeMin", "player_volume_min")
-    .jPlayerId("volumeMax", "player_volume_max")
-    .jPlayerId("volumeBar", "player_volume_bar")
-    .jPlayerId("volumeBarValue", "player_volume_bar_value")
-    .onProgressChange( function(loadPercent, playedPercentRelative, playedPercentAbsolute, playedTime, totalTime) {
-      var myPlayedTime = new Date(playedTime);
-      var ptMin = (myPlayedTime.getUTCMinutes() < 10) ? "0" + myPlayedTime.getUTCMinutes() : myPlayedTime.getUTCMinutes();
-      var ptSec = (myPlayedTime.getUTCSeconds() < 10) ? "0" + myPlayedTime.getUTCSeconds() : myPlayedTime.getUTCSeconds();
-      $("#play_time").text(ptMin+":"+ptSec);
-
-      var myTotalTime = new Date(totalTime);
-      var ttMin = (myTotalTime.getUTCMinutes() < 10) ? "0" + myTotalTime.getUTCMinutes() : myTotalTime.getUTCMinutes();
-      var ttSec = (myTotalTime.getUTCSeconds() < 10) ? "0" + myTotalTime.getUTCSeconds() : myTotalTime.getUTCSeconds();
-      $("#total_time").text(ttMin+":"+ttSec);
-    })
-    .onSoundComplete( function() {
-      Playlist.playListNext();
-    });
-
-    $("#ctrl_prev").click( function() {
-      Playlist.playListPrev();
-      return false;
-    });
-
-    $("#ctrl_next").click( function() {
-      Playlist.playListNext();
-      return false;
-    });
-  };
-  
-  function load_artists() {
+	},
+	
+  loadArtists: function() {
     $.get( '/artists', function(data) {
       $('#content').html(data);
       $('#tiles ul').listnav({showCounts: false});
     });
-  }
+  },
   
-  function show_playlist() {
-    $('#content').css('width', '80%');
-    $('#playlist').show();
-  }
-  
-  function hide_playlist() {
-    $('#content').css('width', '100%');
-    $('#playlist').hide();
-  }
-  
-  function toggle_playlist_visibility() {
-    if( $('#playlist').is(":hidden") == true ) {
-      show_playlist();
-    } else {
-      hide_playlist();
-    }
-  }
-  
-  function load_playlist() {
+  loadPlaylist: function() {
     $.getJSON('/playlist/load', function(data) {
       myPlayList = data;
-			Playlist.displayPlayList();
+			FTANGPlayer.displayPlayList();
     });
+  },
+  
+  showPlaylist: function() {
+    $('#content').css('width', '80%');
+    $('#playlist').show();
+  },
+  
+  hidePlaylist: function() {
+    $('#content').css('width', '100%');
+    $('#playlist').hide();
+  },
+  
+  togglePlaylistVisibility: function() {
+    if( $('#playlist').is(":hidden") == true ) {
+      FTANGPlayer.showPlaylist();
+    } else {
+      FTANGPlayer.hidePlaylist();
+    }
+  },
+
+  appendPlaylistAddButtons: function(cover) {
+    $('.add_album_to_playlist').remove();
+    $('.view_songs_in_album').remove();
+    var add_html = "<div class='view_songs_in_album'></div>" +
+                   "<div class='add_album_to_playlist'>+ Add album to Playlist</div>";
+    $(cover).append(add_html);
+  },
+
+  createAlbumMouseoverTriggers: function() {
+    $('.album').mouseover(function(){
+      FTANGPlayer.appendPlaylistAddButtons(this);
+      FTANGPlayer.resetCoverMouseoverTriggers(this);
+    });
+  },
+
+  resetCoverMouseoverTriggers: function(cover) {
+    $('.album').unbind();
+    FTANGPlayer.createAlbumMouseoverTriggers();
+    $(cover).unbind();
   }
+}
+
+$( function() {
 
   $(".add_album_to_playlist").live("click", function(e) {
     e.preventDefault();
@@ -169,32 +177,11 @@ $( function() {
     var album = $(img).attr('album');
     var artist = $(img).attr('artist');
     $.get('/playlist/add/' + artist + '/' + album, function() {
-      load_playlist();
-      show_playlist();
+      FTANGPlayer.loadPlaylist();
+      FTANGPlayer.showPlaylist();
     });
   });
 
-  function append_playlist_add_buttons (cover) {
-    $('.add_album_to_playlist').remove();
-    $('.view_songs_in_album').remove();
-    var add_html = "<div class='view_songs_in_album'></div>" +
-                   "<div class='add_album_to_playlist'>+ Add album to playlist</div>";
-    $(cover).append(add_html);
-  }
-
-  function create_album_mouseover_triggers() {
-    $('.album').mouseover(function(){
-      append_playlist_add_buttons(this);
-      reset_cover_mouseover_triggers(this);
-    });
-  }
-
-  function reset_cover_mouseover_triggers(cover) {
-    $('.album').unbind();
-    create_album_mouseover_triggers();
-    $(cover).unbind();
-  }
-  
   $(".tiles a").live("click", function(e) {
     e.preventDefault();
     var artist = $.trim($(this).text());
@@ -202,12 +189,12 @@ $( function() {
       $('#content').html(data);
       $('#header_artist').show();
       $('#header_artist h1').text(artist);
-      create_album_mouseover_triggers();
+      FTANGPlayer.createAlbumMouseoverTriggers();
     });
   });
 
   $(".home_nav").live("click", function() {
-    load_artists();
+    FTANGPlayer.loadArtists();
     $('#header_artist h1').text("");
     $('#header_artist').hide();
     $('#header_album h1').text("");
@@ -218,7 +205,7 @@ $( function() {
     $.get('/playlist/clear', function(){
       $('#playlist_list ul').empty();
 			myPlayList = "";
-      hide_playlist();
+      FTANGPlayer.hidePlaylist();
     });
   });
   
@@ -231,13 +218,23 @@ $( function() {
   });
   
   $('#toggle_playlist').live('click', function() {
-    toggle_playlist_visibility();
+    FTANGPlayer.togglePlaylistVisibility();
+  });
+
+  $("#ctrl_prev").live('click', function() {
+   FTANGPlayer.playListPrev();
+    return false;
+  });
+
+  $("#ctrl_next").live('click', function() {
+   FTANGPlayer.playListNext();
+    return false;
   });
   
-  load_artists();
-	load_playlist();
-  hide_playlist();
-  init_jplayer(myPlayList);
+  FTANGPlayer.loadArtists();
+	FTANGPlayer.initJPlayer();
+  FTANGPlayer.hidePlaylist();
+
   try {
   _uacct = "UA-9156272-1";
   urchinTracker();
